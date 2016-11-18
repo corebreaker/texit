70d463b6eb090128bd1e55a6c9a26a4746f905b5
Frédéric Meyer (Corebreaker) <frederic.meyer.77@gmail.com>
2016-11-18 06:25:55 +0300

    Initial import

--- texit.go
+++ texit.go
@@ -1,45 +1,39 @@
 package texit
 
 import (
-  "io/ioutil"
-  "os"
-  "os/exec"
-  "strings"
-  "testing"
+    "io/ioutil"
+    "os"
+    "os/exec"
+    "strings"
+    "testing"
 )
 
 func TestCrashes(t *testing.T) {
-  // Only run the failing part when a specific env variable is set
-  if os.Getenv("BE_CRASHER") == "1" {
-    Crashes(42)
-    return
-  }
+    // Only run the failing part when a specific env variable is set
+    if os.Getenv("BE_CRASHER") == "1" {
+        Crashes(42)
+        return
+    }
 
-  // Start the actual test in a different subprocess
-  cmd := exec.Command(os.Args[0], "-test.run=TestCrashes")
-  cmd.Env = append(os.Environ(), "BE_CRASHER=1")
-  stdout, _ := cmd.StderrPipe()
-  if err := cmd.Start(); err != nil {
-    t.Fatal(err)
-  }
+    // Start the actual test in a different subprocess
+    cmd := exec.Command(os.Args[0], "-test.run=TestCrashes")
+    cmd.Env = append(os.Environ(), "BE_CRASHER=1")
+    stdout, _ := cmd.StderrPipe()
+    if err := cmd.Start(); err != nil {
+        t.Fatal(err)
+    }
 
-  // Check that the log fatal message is what we expected
-  gotBytes, _ := ioutil.ReadAll(stdout)
-  got := string(gotBytes)
-  expected := "It crashes because you gave the answer"
-  if !strings.HasSuffix(got[:len(got)-1], expected) {
-    t.Fatalf("Unexpected log message. Got %s but should contain %s", got[:len(got)-1], expected)
-  }
+    // Check that the log fatal message is what we expected
+    gotBytes, _ := ioutil.ReadAll(stdout)
+    got := string(gotBytes)
+    expected := "It crashes because you gave the answer"
+    if !strings.HasSuffix(got[:len(got)-1], expected) {
+        t.Fatalf("Unexpected log message. Got %s but should contain %s", got[:len(got)-1], expected)
+    }
 
-  // Check that the program exited
-  err := cmd.Wait()
-  if e, ok := err.(*exec.ExitError); !ok || e.Success() {
-    t.Fatalf("Process ran with err %v, want exit status 1", err)
-  }
+    // Check that the program exited
+    err := cmd.Wait()
+    if e, ok := err.(*exec.ExitError); !ok || e.Success() {
+        t.Fatalf("Process ran with err %v, want exit status 1", err)
+    }
 }
-
-Not so readable, definitely feels like a hack, but it does the job.
-
-    Date 18 December 2015
-
-Sounds cool? You can follow me on Twitter @AntoineAugusti
